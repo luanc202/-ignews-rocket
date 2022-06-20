586c735a3baecfca1790b0ffb238f132588bddeb
Luan Alves <barkon32@tuta.io>
2022-06-20 15:22:10 -0300

    concluded manageSubscription data saving to db

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream';
 import Stripe from "stripe";
 import { stripe } from '../../services/stripe';
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
     const chunks = [];
@@ -41,8 +42,24 @@ const webhooks = async (req: NextApiRequest, res:NextApiResponse) => {
         const type = event.type
 
         if (!relevantEvents.has(type)) {
-            console.log('Event received', type);
-            
+            try {
+                switch (type) {
+                    case 'checkout.session.completed':
+
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString()
+                        );
+
+                        break;
+                    default:
+                        throw new Error('Unhandled event.')
+                }
+            } catch (err) {
+                return res.json({ error: 'Wenbhook handler failed.'})
+            }         
         }
 
         res.json({ received: true});
