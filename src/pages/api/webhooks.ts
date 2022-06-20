de62f27fb9e3e8ec9434212d72c483a31ed7d32b
Luan Alves <barkon32@tuta.io>
2022-06-20 14:54:56 -0300

    Fixed warning and edited webhooks

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream';
+import Stripe from "stripe";
+import { stripe } from '../../services/stripe';
 
 async function buffer(readable: Readable) {
     const chunks = [];
@@ -19,17 +21,35 @@ export const config = {
     }
 }
 
-export default async (req: NextApiRequest, res:NextApiResponse) => {
+const relevantEvents = new Set([
+    'checkout.session.completed'
+])
+
+const webhooks = async (req: NextApiRequest, res:NextApiResponse) => {
     if (req.method === 'POST') {
         const buf  = await buffer(req);
+        const secret = req.headers['stripe-signature'];
 
+        let event: Stripe.Event;
 
+        try {
+            event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
+        } catch (err) {
+            return res.status(400).send(`Webhook error: ${err.message}`);
+        }
 
-        res.status(200).json({ ok: true});
+        const type = event.type
+
+        if (!relevantEvents.has(type)) {
+            console.log('Event received', type);
+            
+        }
+
+        res.json({ received: true});
     } else {
         res.setHeader('Allow', 'POST');
         res.status(405).end('Method not allowed')
     }
-
-    
 }
+
+export default webhooks;
